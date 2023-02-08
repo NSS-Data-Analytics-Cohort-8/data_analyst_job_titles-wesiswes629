b989e517a2f04d1981adb5392a2a1ebb56227b4a
Wes Ward <wes.ward629@gmail.com>
2023-02-07 19:26:49 -0600

    q7 answered

--- scripts/data_jobs_scripts.sql
+++ scripts/data_jobs_scripts.sql
@@ -65,6 +65,13 @@ SELECT
 
 -- 7.	Select unique job titles from the data_analyst_jobs table. How many are there?
 
+SELECT 
+	COUNT(DISTINCT title)
+	AS count_unique_jobs
+	FROM data_analyst_jobs;
+	
+-- Answer: There are 881 unique job titles listed.
+
 -- 8.	How many unique job titles are there for California companies?
 
 -- 9.	Find the name of each company and its average star rating for all companies that have more than 5000 reviews across all locations. How many companies are there with more that 5000 reviews across all locations?
